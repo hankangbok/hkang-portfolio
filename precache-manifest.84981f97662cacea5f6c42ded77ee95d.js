dae0e5c3bd7feb7b5c4c321dc9b86586cddedcf7
Hannah Kang <kyouyatamax@gmail.com>
2018-12-18 18:11:40 -0800

    Updates

---
+++ precache-manifest.84981f97662cacea5f6c42ded77ee95d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hkang-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "60a4c68bdccc7b743b75",
+    "url": "/hkang-portfolio/static/js/runtime~main.60a4c68b.js"
+  },
+  {
+    "revision": "e007f305dfbc9235afcd",
+    "url": "/hkang-portfolio/static/js/main.e007f305.chunk.js"
+  },
+  {
+    "revision": "2064f015e8613fdad276",
+    "url": "/hkang-portfolio/static/js/1.2064f015.chunk.js"
+  },
+  {
+    "revision": "e007f305dfbc9235afcd",
+    "url": "/hkang-portfolio/static/css/main.1aff8ca7.chunk.css"
+  },
+  {
+    "revision": "ead0127183f58365510c12acf59d7696",
+    "url": "/hkang-portfolio/index.html"
+  }
+];

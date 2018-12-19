186ea1cefa05f9132a10e893a0413dd36b68d792
Hannah Kang <kyouyatamax@gmail.com>
2018-12-18 22:13:45 -0800

    Getting the basic layout ready

--- src/index.js
+++ src/index.js
@@ -8,12 +8,41 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
+
+// Each projects description should include:
+// Description, technologies used, a deployment link button, a source link button
+class ProjectDescription extends React.Component {
+  render() {
+    return (
+      <div class="project-desc">
+      <h1> Project Description here</h1>
+      <h2>Technologies used</h2>
+      <a> view source</a>
+      <a> view deployment</a>
+    </div>
+    );
+  }
+}
+// // This is the container for the 'card' that will display each project in my list
+class ProjectCard extends React.Component {
+  render() {
+    return(
+      <div class="project-card">
+        <img src="#" alt="Project screenshot will go here"/>
+        <ProjectDescription />
+      </div>
+    );
+  } 
+}
+
 class Base extends React.Component {
   render() {
     return (
-        <div>
+        <div id="base">
           <h1>This is a test</h1>
           <h1 id="the-css-test">Checking that CSS works</h1>
+          <ProjectCard />
+          <ProjectCard />
         </div>
       );
   }

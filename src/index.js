f80429cbf453cba96050f07660ea3d900204d796
Hannah Kang <kyouyatamax@gmail.com>
2018-12-18 17:59:05 -0800

    Checked that CSS/JS/HTML are basically linked

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './main.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+class Base extends React.Component {
+  render() {
+    return (
+        <div>
+          <h1>This is a test</h1>
+          <h1 id="the-css-test">Checking that CSS works</h1>
+        </div>
+      );
+  }
+}
+
+ReactDOM.render(<Base />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA

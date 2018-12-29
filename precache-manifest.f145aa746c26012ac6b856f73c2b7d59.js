a44a81e79b7f75e3c1aaa74a4c18a6c1d30bdcf3
Hannah Kang <kyouyatamax@gmail.com>
2018-12-29 02:01:38 -0800

    Updates

--- precache-manifest.f145aa746c26012ac6b856f73c2b7d59.js
+++ precache-manifest.f145aa746c26012ac6b856f73c2b7d59.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/hkang-portfolio/static/js/runtime~main.60a4c68b.js"
   },
   {
-    "revision": "85bb418d1c0f15d01dde",
-    "url": "/hkang-portfolio/static/js/main.85bb418d.chunk.js"
+    "revision": "0b6fcb836acb29da234b",
+    "url": "/hkang-portfolio/static/js/main.0b6fcb83.chunk.js"
   },
   {
     "revision": "a4001cb7dd9dc0ba9b9c",
     "url": "/hkang-portfolio/static/js/1.a4001cb7.chunk.js"
   },
   {
-    "revision": "85bb418d1c0f15d01dde",
-    "url": "/hkang-portfolio/static/css/main.a302c0f7.chunk.css"
+    "revision": "0b6fcb836acb29da234b",
+    "url": "/hkang-portfolio/static/css/main.11e133bc.chunk.css"
   },
   {
-    "revision": "78b822a7110cce47965f64b7353d36e0",
+    "revision": "a1f11fcb2f95b84e9de21e9fded0f96a",
     "url": "/hkang-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hkang-portfolio/precache-manifest.382c0fc968107a77f161092762152153.js"
+  "/hkang-portfolio/precache-manifest.f145aa746c26012ac6b856f73c2b7d59.js"
 );
 
 workbox.clientsClaim();

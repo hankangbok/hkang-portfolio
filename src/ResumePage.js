f408973a2763219845dc0fdd68379f241123b32d
Hannah Kang <kyouyatamax@gmail.com>
2018-12-29 02:10:03 -0800

    change resume link

--- src/ResumePage.js
+++ src/ResumePage.js
@@ -10,7 +10,7 @@ export class ResumePage extends React.Component {
           Yes!
           <br />
           My resume is available{" "}
-          <a href="https://drive.google.com/file/d/1MS2WASHiX0ZkqGm_-21dhjG7xRGw_r1z/view?usp=sharing">
+          <a href="https://drive.google.com/file/d/1uFLY0mdue-Udl6BCpJvQtiSocr_NyBZ7/view?usp=sharing">
             on Google Drive.
           </a>
         </h3>
